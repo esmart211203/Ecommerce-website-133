b46cf7009ee7f05a1149d1a10f112dfabaa92710
esmart211203 <esmart211203@gmail.com>
2024-03-15 14:09:50 +0700

    feat: authentication

--- wsite/urls.py
+++ wsite/urls.py
@@ -4,8 +4,12 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     #### ADMIN ####
-    path("view-admin", views.admin_view, name="admin_index"),
+    path("view-admin", views.admin_view, name="admin:index"),
     path("product", views.product_view, name="product"),
     path("category", views.category_view, name="category"),
     path("user", views.user_view, name="user"),
+    ### AUTH ###
+    path("login", views.LoginView.as_view(), name="login"),
+    path("register", views.RegisterView.as_view(), name="register"),
+    path("logout", views.logout_user, name="logout"),
 ]
--- wsite/views.py
+++ wsite/views.py
@@ -1,11 +1,57 @@
-from django.shortcuts import render,HttpResponse
-
-# Create your views here.
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.views import View
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.http import HttpResponseForbidden
+from .forms import RegistrationForm
+from django.contrib.auth.models import User
+######## FRONT-END #######
 def index(request):
     return render(request, 'index.html')
 
+@login_required(login_url="login")
+@user_passes_test(lambda u: u.is_superuser, login_url="login")
+def is_admin(request):
+    if request.user.is_superuser:
+        return redirect("admin:index")
+    else:
+        return HttpResponseForbidden("Access Denied")
+########## AUTH ##########
+class LoginView(View):
+    def get(self, request):
+        return render(request, "auth/login.html")
 
-#### ADMIN ####
+    def post(self, request):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user is None:
+            context = {
+                "error_message": "Đăng nhập không thành công. Vui lòng kiểm tra lại tên người dùng và mật khẩu."
+            }
+            return render(request, "auth/login.html", context)
+        login(request, user)
+        return redirect("index")
+class RegisterView(View):
+    def get(self, request):
+        form = RegistrationForm()
+        return render(request, "auth/register.html", {"form": form})
+
+    def post(self, request):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(username=username, email=email, password=password)
+            login(request, user)
+            return redirect("index")
+        else:
+            return render(request, "auth/register.html", {"form": form})
+def logout_user(request):
+    logout(request)
+    return redirect('index')
+########## ADMIN #########
 def admin_view(request):
     return render(request, 'admin/index.html')
 
@@ -18,3 +64,5 @@ def category_view(request):
 
 def user_view(request):
     return render(request, 'admin/user/index.html')
+
+
